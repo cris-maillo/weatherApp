aeeab386ae7e2a4d42ba3115a3c3336356126590
cris-maillo <cristinamaillo.r@gmail.com>
2023-03-25 23:35:48 +0000

    created app

---
+++ app.js
@@ -0,0 +1,28 @@
+async function getData(city){
+
+    const apiKey = "b55d634c4bf74d49bb0215431232503";
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        presentData(data)
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+document.querySelector("form").addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const city = document.getElementById("city").value;
+    await getData(city);
+});
+
+function presentData(data){
+    document.getElementById("condition").innerHTML = data.current.condition.text;
+    document.getElementById("feel-temp").innerHTML = data.current.feelslike_c + " °C";
+    document.getElementById("temp").innerHTML = data.current.temp_c + " °C";
+    document.getElementById("humidity").innerHTML = data.current.humidity + " %";
+    document.getElementById("location").innerHTML = data.location.name + ", " + data.location.country;
+}
